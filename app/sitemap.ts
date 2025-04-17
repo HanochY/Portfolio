5b03aad253e42e3084944be3d494f756cc3a688b
HanochY <hanochyarkoni@gmail.com>
2025-04-17 17:48:31 +0300

    add projects to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ export const baseUrl = 'https://localhost'
 
 export default async function sitemap() {
 
-  let routes = [''].map((route) => ({
+  let routes = ['', 'projects'].map((route) => ({
     url: `${baseUrl}${route}`,
     lastModified: new Date().toISOString().split('T')[0],
   }))
